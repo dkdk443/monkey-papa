b36d429e73998bae8af704fdad5b2f79d4949fd5
dkdk443 <demo_weeeds_470@yahoo.co.jp>
2024-08-17 13:09:17 +0900

    お知らせをgoogle spreadsheetから取得するように変更

--- src/components/news.js
+++ src/components/news.js
@@ -1,18 +1,59 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { motion } from "framer-motion"
-import { useNewsMetadata } from '../hooks/news-meta-data';
+import axios from 'axios'
+const userContentKey = process.env.GATSBY_USER_CONTENT_KEY;
 
 const News = () => {
-  const newsList = useNewsMetadata();
-  let displayNewsList = newsList
+  const [news, setNews] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const url = `https://script.googleusercontent.com/macros/echo?user_content_key=${userContentKey}`;
+      try {
+        const response = await axios.get(url);
+        setNews(response.data);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+    fetchData();
+  }, []);
+
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1; // 月は0から始まるため +1
+    const day = date.getDate();
+    const daysOfWeek = ['日', '月', '火', '水', '木', '金', '土'];
+    const dayOfWeek = daysOfWeek[date.getDay()]; // 曜日を取得
+
+    return `${year}年${month}月${day}日(${dayOfWeek})`;
+  }
+
+  const formatTime = (timeString) => {
+    // Dateオブジェクトを作成
+    const date = new Date(timeString);
+
+    // 日本時間 (JST) に変換
+    const jstHours = date.getUTCHours() + 9; // UTC + 9 時間で日本標準時に変換
+    const minutes = date.getMinutes();
+
+    // 時刻を "HH:MM" 形式にフォーマット
+    const formattedTime = `${String(jstHours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}`;
+
+    return formattedTime;
+  }
+
+
+  let displayNewsList = news
+    .filter(news => news.is_visible === true)
     .filter(news => {
       let now = new Date();
-      let end_date = new Date(news.frontmatter.end_date);
+      let end_date = new Date(news.end_date);
       return end_date >= now;
     })
     .sort((a, b) => {
-      let aNews = a.frontmatter.start_date;
-      let bNews = b.frontmatter.start_date;
+      let aNews = a.start_date;
+      let bNews = b.start_date;
       if (aNews < bNews) {
         return -1;
       }
@@ -37,18 +78,19 @@ const News = () => {
             bounce: 0.4,
             duration: 1,
             delay: 0.6,
-        }}}
+          }
+        }}
       >
         <div className="detail">
-            現在お知らせはありません
+          現在お知らせはありません
         </div>
       </motion.li>
     );
   } else {
-    return displayNewsList.map(news =>
+    return displayNewsList.map((news, index) =>
       <motion.li
         className='news-item'
-        key={news.id}
+        key={index}
         initial={{
           opacity: 0,
           y: 40,
@@ -61,50 +103,51 @@ const News = () => {
             bounce: 0.4,
             duration: 1,
             delay: 0.6,
-        }}}
+          }
+        }}
       >
         <div className="detail">
-          <span className="label">{ news.frontmatter.label }</span>
-          <div className="title">{ news.frontmatter.title }</div>
+          <span className="label">{news.label}</span>
+          <div className="title">{news.title}</div>
           <div className="s-title">■ 日時</div>
           <div className="s-section">
             <div className="date"
               style={{
-                fontSize:"20px"
+                fontSize: "20px"
               }}
-            >{news.frontmatter.start_date}~{news.frontmatter.end_date}</div>
+            >{formatDate(news.start_date)}~{formatDate(news.end_date)}</div>
             <div
               className="time"
               style={{
-              fontSize: "20px"
-            }}>{news.frontmatter.start_time}~{news.frontmatter.end_time}</div>
+                fontSize: "20px"
+              }}>{formatTime(news.start_time)}~{formatTime(news.end_time)}</div>
             {
-              news.frontmatter.date_time_note.map((note, index) => 
-                <div key={index} className="small-text"> ※{ note }</div>
+              news.date_time_note.map((note, index) =>
+                <div key={index} className="small-text"> ※{note}</div>
               )
             }
           </div>
           <div className="s-title">■ 場所</div>
           <div className="s-section">
             <div className="place-name">
-              { news.frontmatter.place_name }
+              {news.place_name}
             </div>
             <div className="address">
-              { news.frontmatter.address }
+              {news.address}
             </div>
             <div className="place" style={{
               marginTop: '20px',
             }}>
               <iframe className='google-map'
-                src={news.frontmatter.map_url}
+                src={news.map_url}
                 style={{ border: 0 }}
                 allowFullScreen={true}
                 loading="lazy" referrerPolicy="no-referrer-when-downgrade"
-                title={news.frontmatter.address}
+                title={news.address}
                 height={300}
               ></iframe>
             </div>
-            <div>{ news.frontmatter.tell }</div>
+            <div>{news.tell}</div>
           </div>
           {/* <div className="s-title">■ 内容</div>
           <div className="s-section">
